15810d9596bf23b6090ed2dd2ed031abfcc83025
ThoratKiran1210 <127298235+ThoratKiran1210@users.noreply.github.com>
2023-05-24 08:29:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('https://api.example.com/movies'); 
+        // Replace with your API endpoint
+        setMovies(response.data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Movie Recommendation App</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
